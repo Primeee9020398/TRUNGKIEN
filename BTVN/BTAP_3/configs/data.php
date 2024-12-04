6f8b3cf271b2b173974b3c3b84bb1e34bebd7985
Trung Kiin <136331695+Primeee9020398@users.noreply.github.com>
2024-12-04 09:23:21 +0700

    Add files via upload

--- BTVN/BTAP_3/configs/data.php
+++ BTVN/BTAP_3/configs/data.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_HOST', 'localhost'); 
-define('DB_USER', 'minthu');      
-define('DB_PASS', '123456789');         
+define('DB_USER', 'trungkien');      
+define('DB_PASS', 'kien2004');         
 define('DB_NAME', 'QLHoa'); 
 function getConnection() {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
