d7113bee0de5df01492aa8efc68d605edfc02448
Primeee9020398 <trungkienn10a6@gmail.com>
2024-12-18 01:21:22 +0700

    First commit

---
+++ BTAP_LARAVEL/Laravel_01/database/seeders/it_centersSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class it_centersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
